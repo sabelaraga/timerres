ad4d0ba6cca5322d97f3652709cbb4d00dd8ffe9
Sabela Ramos Garea <rsabela@greina30.cm.cluster>
2016-02-18 19:47:05 +0100

    basic codes

---
+++ timerres.c
@@ -0,0 +1,91 @@
+#include "util.h"
+
+
+
+void benchmark (int work){
+	uint64_t ** mytimes;
+	int i=0,j=0,spurious=0,k=0;
+	uint64_t *variances;
+	uint64_t *min_values;
+	uint64_t max_dev =0, min_time=0, max_time=0, prev_min=0, tot_var=0, max_dev_all=0, var_of_vars=0, var_of_mins=0; 
+	
+	mytimes =(uint64_t**) malloc(BOUND_OF_LOOP*sizeof(uint64_t*));
+	if(!mytimes){
+		fprintf(stderr,"Unable to allocate memory for mytimes \n");
+		return;
+	}
+	
+	for(j=0; j<BOUND_OF_LOOP; j++){
+		mytimes[j] = (uint64_t*)malloc(SIZE_OF_STAT*sizeof(uint64_t));
+		if(!mytimes[j]){
+			fprintf(stderr,"Unable to allocate memory for mytimes[%d] \n",j);
+			return;
+		}
+	}
+	
+	variances = malloc(BOUND_OF_LOOP*sizeof(uint64_t));
+	if(!variances){
+		fprintf(stderr,"Unable to allocate memory for variances \n");
+		return;
+	}
+	min_values = malloc(BOUND_OF_LOOP*sizeof(uint64_t));
+	if(!min_values){
+		fprintf(stderr,"Unable to allocate memory for min_values \n");
+		return;
+	}
+
+	if(!work) Filltimes_empty(mytimes);
+	else	  Filltimes(mytimes);
+
+	for(j=0; j<BOUND_OF_LOOP; j++){
+		max_dev=0;
+		min_time=0;
+		max_time=0;
+
+		for(i=0;i<SIZE_OF_STAT;i++){
+			if(((min_time==0)||(min_time>mytimes[j][i]))&&(mytimes[j][i]!=0)) min_time=mytimes[j][i];
+			if(max_time<mytimes[j][i]) max_time = mytimes[j][i];
+		}
+		max_dev = max_time-min_time;
+		min_values[j] = min_time;
+
+		if((prev_min!=0)&&(prev_min>min_time)) spurious++;
+		if(max_dev > max_dev_all) max_dev_all = max_dev;
+
+		variances[j] = var_calc(mytimes[j],SIZE_OF_STAT);
+		tot_var += variances[j];
+
+		fprintf(stdout,"\nloop_size %d >>>> variance(cycles): %llu; max_deviation: %llu; min time: %llu", j,variances[j],max_dev,min_time);
+
+		prev_min = min_time;
+	}
+
+	var_of_vars = var_calc(variances,BOUND_OF_LOOP);
+	var_of_mins = var_calc(min_values,BOUND_OF_LOOP);
+
+
+	fprintf(stdout, "\n total number of spurious min values = %d", spurious);
+	fprintf(stdout, "\n total variance = %llu", (tot_var/BOUND_OF_LOOP));
+	fprintf(stdout, "\n absloute max deviation = %llu", max_dev_all);
+	fprintf(stdout, "\n variance of variances = %llu", var_of_vars);
+	fprintf(stdout, "\n variance of minimum values = %llu", var_of_mins);
+	fprintf(stdout, "\n");
+
+	for(j=0; j<BOUND_OF_LOOP; j++) free(mytimes[j]);
+	free(mytimes);
+	free(variances);
+	free(min_values);
+	return;
+}
+	
+
+
+
+int main(int argc, char** argv){
+	fprintf(stdout,"STARTING EMPTY BENCHMARK\n");	
+	benchmark(0);
+	fprintf(stdout,"EMPTY BENCHMARK FINALIZED\n");
+	fprintf(stdout,"STARTING WORK BENCHMARK\n");	
+	benchmark(1);
+	fprintf(stdout,"WORK BENCHMARK FINALIZED\n");
+}
---
+++ util.h
@@ -0,0 +1,184 @@
+#ifndef __UTIL_RDTSC__
+#define __UTIL_RDTSC__
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct {
+	uint32_t l;
+	uint32_t h;
+} rdtsc_timeval_t;
+
+typedef struct{
+	rdtsc_timeval_t t1;
+	rdtsc_timeval_t t2;
+} mytimer_t;
+
+uint64_t inline getCycles (rdtsc_timeval_t t){
+	return ( ((uint64_t)t.h<<32) | t.l);
+}
+
+#define __cpuid(level, a, b, c, d)              \
+ __asm__ __volatile__("mov %%rbx, %%rdi;"			\
+	  "cpuid\n\t"                           \
+	  "mov %%rbx, %%rsi;"			\
+	  "mov %%rdi, %%rbx;"			\
+          : "=a" (a), "=S" (b), "=c" (c), "=d" (d)     \
+          : "0" (level)			\
+	  :"rdi");
+
+
+#define RDTSC_GET_TIMESTAMP(t1)  __asm__ __volatile__ ("rdtsc" : "=a" (t1.l), "=d" (t1.h));
+#define RDTSCP_GET_TIMESTAMP(t1)  __asm__ __volatile__ ("rdtscp" : "=a" (t1.l), "=d" (t1.h));
+
+//from the Intel paper. They segfault after an undefined number of iterations because it is not taking care of all registers.
+//Anyway, the variance is similar to the other version
+//#define RDTSC_GET_TIMESTAMP(t1)  __asm__ __volatile__ (	"cpuid\n\t"\
+							"rdtsc\n\t"\
+							"mov %%edx, %0\n\t"\
+							"mov %%eax, %1\n\t"\
+							 : "=r" (t1.h), "=r" (t1.l)::"%rax","%rbx","%rcx","%rdx");
+//#define RDTSCP_GET_TIMESTAMP(t1)  __asm__ __volatile__ ("rdtscp\n\t"\
+							"mov %%edx, %0\n\t"\
+							"mov %%eax, %1\n\t"\
+							"cpuid\n\t"\
+							: "=r" (t1.h), "=r" (t1.l)::"rax","rbx","rcx","rdx");
+
+
+
+
+
+#define SIZE_OF_STAT 100000
+#define BOUND_OF_LOOP 1000
+
+
+void inline Filltimes_empty (uint64_t ** times){
+	uint64_t a,b,c,d;
+	mytimer_t timer;
+	int i,j;	
+	uint64_t start, end;
+
+	//warmup instruction cache
+	__cpuid(0,a,b,c,d);
+	RDTSC_GET_TIMESTAMP (timer.t1);
+	RDTSCP_GET_TIMESTAMP(timer.t2);
+	__cpuid(0,a,b,c,d);
+
+	__cpuid(0,a,b,c,d);
+	RDTSC_GET_TIMESTAMP (timer.t1);
+	RDTSCP_GET_TIMESTAMP(timer.t2);
+	__cpuid(0,a,b,c,d);
+
+	for(j=0; j<BOUND_OF_LOOP; j++){
+		for(i=0; i<SIZE_OF_STAT; i++){
+			__cpuid(0,a,b,c,d);
+			RDTSC_GET_TIMESTAMP (timer.t1);
+			//Insert the function to measure here
+			RDTSCP_GET_TIMESTAMP(timer.t2);
+			__cpuid(0,a,b,c,d);
+			
+			start = getCycles(timer.t1);
+			end   = getCycles(timer.t2);
+			
+			if((end-start)<0){
+				fprintf(stderr,"ERROR IN TAKING THE TIME:\n \tloop(%d) \tstat(%d) \tstart= %llu, \tend=%llu\n", j,i,start,end);
+				times[j][i]=0;
+			}
+			else{
+				times[j][i]=end-start;
+			}
+		}
+	}
+	return;
+}
+
+void inline measured_loop(unsigned int n, volatile int *var){
+	int k;
+	for(k=0; k<n; k++){
+		(*var)=1;
+	}
+}
+
+
+void inline Filltimes (uint64_t ** times){
+	uint64_t a,b,c,d;
+	mytimer_t timer;
+	int i,j;	
+	volatile int variable = 0; 
+	uint64_t start, end;
+
+	//warmup instruction cache
+	__cpuid(0,a,b,c,d);
+	RDTSC_GET_TIMESTAMP (timer.t1);
+	RDTSCP_GET_TIMESTAMP(timer.t2);
+	__cpuid(0,a,b,c,d);
+
+	__cpuid(0,a,b,c,d);
+	RDTSC_GET_TIMESTAMP (timer.t1);
+	RDTSCP_GET_TIMESTAMP(timer.t2);
+	__cpuid(0,a,b,c,d);
+
+	for(j=0; j<BOUND_OF_LOOP; j++){
+		for(i=0; i<SIZE_OF_STAT; i++){
+			variable = 0;
+			__cpuid(0,a,b,c,d);
+			RDTSC_GET_TIMESTAMP (timer.t1);
+			measured_loop(j, &variable);
+			RDTSCP_GET_TIMESTAMP(timer.t2);
+			__cpuid(0,a,b,c,d);
+			
+			start = getCycles(timer.t1);
+			end   = getCycles(timer.t2);
+			
+			if((end-start)<0){
+				fprintf(stderr,"ERROR IN TAKING THE TIME:\n \tloop(%d) \tstat(%d) \tstart= %llu, \tend=%llu, \tvariable=%u\n", j,i,start,end,variable);
+				times[j][i]=0;
+			}
+			else{
+				times[j][i]=end-start;
+			}
+		}
+	}
+	return;
+}
+
+
+uint64_t var_calc(uint64_t *inputs, int size){
+	int i;
+	uint64_t acc =0, previous = 0, temp_var =0;
+	for(i=0; i<size; i++){
+		if(acc<previous){
+			fprintf(stderr,"OVERFLOW ERROR IN var_calc\n");
+			return -1;	
+		}
+		previous=acc;
+		acc += inputs[i];
+	}
+	acc *= acc;
+	
+	if(acc<previous){
+		fprintf(stderr,"OVERFLOW ERROR IN var_calc\n");
+		return -1;	
+	}
+	previous = 0;
+	for(i=0; i<size; i++){
+		if(temp_var<previous){
+			fprintf(stderr,"OVERFLOW ERROR IN var_calc\n");
+			return -1;	
+		}
+		previous = temp_var;
+		temp_var += (inputs[i]*inputs[i]);
+	}	
+	temp_var =temp_var*size;
+	if(temp_var<previous){
+		fprintf(stderr,"OVERFLOW ERROR IN var_calc\n");
+		return -1;	
+	}
+	temp_var=(temp_var-acc)/(((uint64_t)(size))*((uint64_t)(size)));
+	return (temp_var);
+}
+
+
+
+#endif
